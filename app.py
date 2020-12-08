68ced5bdb97ca5307e32e6e48440f661ffd3cd34
belsiboy <75416317+belsiboy@users.noreply.github.com>
2020-12-07 18:55:39 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11,GPIO.OUT)
+servo1 = GPIO.PWM(11,50)
+servo1.start(0)
+
+GPIO.setwarnings(False)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/lightson')
+def lightson():
+    GPIO.output(11, True)
+    servo1.ChangeDutyCycle(120/18 +2)
+    sleep(2)
+    servo1.ChangeDutyCycle(90/18 +2)
+    sleep(2)
+    servo1.ChangeDutyCycle(0)
+    GPIO.output(11, False)
+    return render_template('index.html')
+
+
+@app.route('/lightsoff')
+def lightsoff():
+    GPIO.output(11, True)
+    servo1.ChangeDutyCycle(60/18 +2)
+    sleep(2)
+    servo1.ChangeDutyCycle(90/18 +2)
+    sleep(2)
+    servo1.ChangeDutyCycle(0)
+    GPIO.output(11, False)
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
